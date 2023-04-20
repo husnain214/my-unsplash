99972ef136920a96ffb1984b2992b929653d4da8
Husnain Zahid <husnainzahid214@gmail.com>
2023-04-20 00:27:06 -0700

    'added favicon for dark mode and added masonry plugin for cross browser compatibility'

--- frontend/src/components/ImageGallery.js
+++ frontend/src/components/ImageGallery.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
+import Masonry from 'react-masonry-css'
 
 import './ImageGallery.css'
 
@@ -13,20 +14,33 @@ const ImageGallery = ({ verifyUser, deleteImage, images }) => {
   const openDeletePhotoModal = () => {
     imageGalleryRef.current.openDialog()
   }
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1
+  }
   
   return (
     <>
       <main className='image-gallery | container'>
-        {
-          images.map(image =>
-            <Image 
-              key = {image.id}
-              image = {image}
-              setDeleteImage = {setDeleteImage}
-              openDeletePhotoModal = {openDeletePhotoModal} 
-            />
-          )
-        }
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {
+            images.map(image =>
+              <Image 
+                key = {image.id}
+                image = {image}
+                setDeleteImage = {setDeleteImage}
+                openDeletePhotoModal = {openDeletePhotoModal} 
+              />
+            )
+          }
+        </Masonry>
       </main>
 
       <DeleteModal 
